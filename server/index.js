e7dcfe27b9c4e4633bcc73bc243914e0cf01f208
spirit0707 <molycheva07@ya.ru>
2025-05-17 02:46:23 +0300

    add error handling, file uploads & offer management

--- server/index.js
+++ server/index.js
@@ -2,18 +2,25 @@ import express from 'express';
 import * as dotenv from 'dotenv'
 import sequelize from './config/database.js';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { router } from './routes/index.js';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
 
 
 dotenv.config()
 
-const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
 app.use('/', router);
+app.use(errorMiddleware);
 
 
 const start = async () => {
--- server/routes/offerRoutes.js
+++ server/routes/offerRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getAllOffers } from "../controllers/offerController.js";
+import upload from "../middleware/upload.js";
+import { createOffer, getAllOffers } from "../controllers/offerController.js";
 
 const router = new Router();
 
 router.get('/offers', getAllOffers);
+router.post('/offers', upload.fields([
+    { name: 'previewImage', maxCount: 1 },
+    { name: 'photos', maxCount: 6 }
+    ]), createOffer);
 
 export default router;
